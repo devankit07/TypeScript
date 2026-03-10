adf1c47956494c46c99ac5d0d5fcd11b617afbb6
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-03-10 21:34:46 +0530

    types&array

--- src/basicTypes.ts
+++ src/basicTypes.ts
@@ -1,1 +1,11 @@
-let name:string="user"
+// types - primitive
+let name:string="user";
+let age:number=12;
+let isstdent:boolean=true;
+let notsure:any="could be anything num,str,{}";
+
+// array
+
+let numbers:number[]=[1,2,3];
+let strings:Array<string>=['sohil','naman'];
+let mixed:(number | string | boolean)[]=["user",24,true]
